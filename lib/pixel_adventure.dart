0446ac2ad5d6cff11be3afed9fd58616763e4032
Jungsik Won <wl808@naver.com>
2023-11-24 21:57:23 +0900

    2023 11 24

---
+++ lib/pixel_adventure.dart
@@ -0,0 +1,23 @@
+import 'dart:async';
+
+import 'package:flame/components.dart';
+import 'package:flame/game.dart';
+import 'package:pixel_adventure/levels/level.dart';
+
+class PixelAdventure extends FlameGame {
+  late final CameraComponent cam;
+
+  @override
+  final world = Level();
+
+  @override
+  FutureOr<void> onLoad() {
+    cam = CameraComponent.withFixedResolution(
+        world: world, width: 640, height: 360);
+
+    cam.viewfinder.anchor = Anchor.topLeft;
+
+    addAll([cam, world]);
+    return super.onLoad();
+  }
+}
