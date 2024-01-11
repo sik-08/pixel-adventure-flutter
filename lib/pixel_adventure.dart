68d2a812d76eb6d0d5d4cf5eb33d3d986b8157bc
Jungsik Won <wl808@naver.com>
2024-01-12 02:10:15 +0900

    2024 01 12

--- lib/pixel_adventure.dart
+++ lib/pixel_adventure.dart
@@ -15,7 +15,10 @@ class PixelAdventure extends FlameGame {
   final world = Level();
 
   @override
-  FutureOr<void> onLoad() {
+  FutureOr<void> onLoad() async {
+    // Load all images into cache
+    await images.loadAllImages();
+
     cam = CameraComponent.withFixedResolution(
         world: world, width: 640, height: 360);
 
