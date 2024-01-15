472b9ebba6e032e808bfc15701ea3ea864d735f2
Jungsik Won <wl808@naver.com>
2024-01-16 02:48:06 +0900

    2024 01 16 Add support for Desktop keyboard control

--- lib/pixel_adventure.dart
+++ lib/pixel_adventure.dart
@@ -3,9 +3,10 @@ import 'dart:ui';
 
 import 'package:flame/components.dart';
 import 'package:flame/game.dart';
+import 'package:flame/input.dart';
 import 'package:pixel_adventure/levels/level.dart';
 
-class PixelAdventure extends FlameGame {
+class PixelAdventure extends FlameGame with HasKeyboardHandlerComponents {
   @override
   Color backgroundColor() => const Color(0xFF211F30);
 
